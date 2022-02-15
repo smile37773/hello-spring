ce75d7bce6907d5fb4059ca95b05bd43a9a32c6f
Qingyi Liu <qingyliu@microsoft.com>
2022-02-15 14:24:46 +0800

    add files

---
+++ src/main/java/com/localtest/hellospring/HelloController.java
@@ -0,0 +1,17 @@
+package com.localtest.hellospring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class HelloController {
+    /**
+     * http://localhost:8080/hello
+     */
+    @GetMapping("/hello")
+    public String world() {
+        return "Hello, world @" + new Date().toString() + "!";
+    }
+}
